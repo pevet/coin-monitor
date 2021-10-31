d304563413296c16eeb23b3303b37f498c4c2833
Peter Vegh <r2b@pvegh.com>
2021-10-31 19:45:27 +0100

    getting rid of createApp function

--- src/multi-stream-depth.js
+++ src/multi-stream-depth.js
@@ -24,46 +24,44 @@ let db = mysql.createConnection({
   database: "coinscanner"
 });
 
-export default async function createApp() {
-  let msgType='24hrTicker';
-  logger.debug('Start application for '+msgType);
-  var pairs;
-  var socketApi;
-  db.connect(function(err) {
+let msgType='24hrTicker';
+logger.debug('Start application for '+msgType);
+var pairs;
+var socketApi;
+db.connect(function(err) {
+  if (err) throw err;
+  logger.debug("Database Connected!");
+
+  var sql = "SELECT symbol FROM pairs WHERE active = 1";
+  db.query(sql, function (err, result) {
     if (err) throw err;
-    logger.debug("Database Connected!");
 
-    var sql = "SELECT symbol FROM pairs WHERE active = 1";
-    db.query(sql, function (err, result) {
-      if (err) throw err;
-
-      pairs = result.map((row) => `${row.symbol}@ticker`).join('/');
-      pairs = pairs.toLowerCase();
+    pairs = result.map((row) => `${row.symbol}@ticker`).join('/');
+    pairs = pairs.toLowerCase();
 //      logger.debug("5#"+pairs+"#");
 
-      socketApi = subscribeToStream(pairs,msgType);
-
-      setInterval(() => {
-        if (socketApi._ws.readyState === WebSocket.CLOSED) {
-          logger.debug("Websocket is closed, trying to resubscribe");
-          socketApi = subscribeToStream(pairs,msgType);
-        }
-      }, 2000);
-
-      setInterval(() => { //force close the stream after 12 hrs and re-subscribe to keep stream alive
-        logger.debug("Closing stream");
-        socketApi._ws.close();
-      }, 12*60*60*1000);
-    });
+    socketApi = subscribeToStream(pairs,msgType);
   });
-}
+});
+
+setInterval(() => {
+  if (socketApi._ws.readyState === WebSocket.CLOSED) {
+    logger.debug("Websocket is closed, trying to resubscribe");
+    socketApi = subscribeToStream(pairs,msgType);
+  }
+}, 2000);
+
+setInterval(() => { //force close the stream after 12 hrs and re-subscribe to keep stream alive
+  logger.debug("Closing stream");
+  socketApi._ws.close();
+}, 12*60*60*1000); //12 hours
 
 function subscribeToStream(pairs, msgType) {
-  const socketApi = new SocketClient(`stream?streams=${pairs}`);
-  socketApi.setHandler(msgType, (params) => storeTicker(params));
+  const sApi = new SocketClient(`stream?streams=${pairs}`);
+  sApi.setHandler(msgType, (params) => storeTicker(params));
   logger.debug("subscribed to stream");
-  return socketApi;
-}
+  return sApi;
+};
 
 function storeTicker(params) {
   logger.info(params);
@@ -76,6 +74,4 @@ function storeTicker(params) {
     }
     logger.info("Inserted: "+sql);
   });
-}
-
-createApp();
+};
